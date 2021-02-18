3cd8b28e8926874df00340c4740ae396c0e2f7a7
Kévin Blayassi <bilbo12@live.fr>
2021-02-18 09:46:58 +0100

    MaJ Brouillon

--- src/Brouillon.java
+++ src/Brouillon.java
@@ -12,6 +12,44 @@ public class Brouillon {
 		}
 		return codeSecret;
 	}
+	
+	public int[] evaluerProposition(Color[] P, Color[] S, int nbCoul) {
+		int nb=P.length;
+		int noir=0;
+		
+		for (int i=0; i<nb; i++) {
+			if (P[i]==S[i]) {
+				noir++;
+			}
+		}
+		
+		int blanc = -noir;
+		
+		for (int i=0; i<nbCoul; i++) {
+			int n=0;
+			int m=0;
+			for (int j=0; j<nb; j++) {
+				if (S[j]==this.COULEUR[i]) {
+					n+=1;
+				}
+				if (P[j]==this.COULEUR[i]) {
+					m+=1;
+				}
+			}
+			if (n<m) {
+				blanc+=n;
+			} else {
+				blanc+=m;
+			}
+		}
+		
+		int[] result = {noir, blanc};
+		
+		return result;
+		
+		
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
